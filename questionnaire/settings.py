b7a755f80e781b0b840d6b37ecf5cd43eab8c149
Mikko Johansson <mikko.johansson@aalto.fi>
2012-01-19 17:30:17 +0200

    Added OpenLayers GoogleMap as basemap, Very early commit

--- questionnaire/settings.py
+++ questionnaire/settings.py
@@ -20,7 +20,7 @@ DATABASES = {
         'USER': 'sample_user',                      # Not used with sqlite3.
         'PASSWORD': 'sample',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '5433',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
