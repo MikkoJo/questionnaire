786f3aabe2646f6b7c73625f73d32c8db17aafa6
Mikko Johansson <mikko.johansson@aalto.fi>
2012-03-28 18:09:22 +0100

    added staticfiles to the settings

--- questionnaire/settings.py
+++ questionnaire/settings.py
@@ -61,13 +61,13 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'static'),
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media'),
 MEDIA_ROOT = MEDIA_ROOT[0]
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = "/static/"
+MEDIA_URL = "/media/"
 
 CACHES = getattr(settings_local, "CACHES", {
     'default': {
@@ -117,7 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.admin',
     'django.contrib.gis',
-#    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 
     # softgis-api applications
     #'api.softgis_client',
@@ -143,11 +143,20 @@ USE_MONGODB = getattr(settings_local, "USE_MONGODB", True)
 
 # Spatial reference system identity (srid) of the database
 # 3067 EUREF_FIN_TM35FIN (ETRS89 / ETRS-TM35FIN)
+# 3857 Web mercator, same as 900913, 3857 is the official EPSG code
 SPATIAL_REFERENCE_SYSTEM_ID = 3857
 
 STATIC_ROOT = getattr(settings_local, "STATIC_ROOT", '')
 STATIC_URL = getattr(settings_local, "STATIC_URL", '/static/')
 STATICFILES_DIRS = getattr(settings_local, "STATICFILES_DIRS", ('',))
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 #email smtp configuration
 EMAIL_HOST = getattr(settings_local, 'EMAIL_HOST', '')
--- questionnaire/settings_tuusula.py
+++ questionnaire/settings_tuusula.py
@@ -12,7 +12,7 @@ LANGUAGES = (('fi', 'finnish'),)
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', #gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hyvinkaa',                      # Or path to database file if using sqlite3.
+        'NAME': 'tuusula',                      # Or path to database file if using sqlite3.
         'USER':  getattr(settings_local, 'DATABASE_USER', ''), # Not used with sqlite3.
         'PASSWORD': getattr(settings_local, 'DATABASE_PW', ''), # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
